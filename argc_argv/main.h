01197f951a191a09e6d58bf62e9fcb2dde390ace
Kamogelo Modise <kamokagisomodise@gmail.com>
2025-06-14 16:27:18 +0200

    main.h

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,15 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for 0-whatsmyname.c */
-int main(int argc, char *argv[]);
+int _putchar(char c);
+int _atoi(char *s);
 
-
-
-
-
-
-
-
-
-#endif /* MAIN_H */
+#endif
